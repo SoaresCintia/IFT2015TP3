3c219f5a9dab1630c80dc97d0e23c942d75b99cb
larry147 <fotsoguiffo8@gmail.com>
2023-07-28 15:47:26 -0400

    adding edges to the graph

--- Graph.java
+++ Graph.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Graph {
+
+     private int curIndex = 0;
+    // // clé – sommet, valeur – indice de sommet 
+     private TreeMap<String, Integer> indexes;
+    // private ArrayList<Vertex> vertexs;
+
+    private TreeSet<String> vertexTree = new TreeSet<>();
+    private ArrayList<Vertex> vertexsEdges = new ArrayList<Vertex>();
 
     class Edge{
         
         String name;
-        String first, second;
+        Vertex first, second;
         int value;
 
-        public Edge(String name, String first, String second, int value) {
+        public Edge(String name, Vertex first, Vertex second, int value) {
             this.name = name;
             this.first = first;
             this.second = second;
@@ -22,33 +31,62 @@ public class Graph {
         // e(v,w) sortante. s = v
         // clé – indice de w dans une table des sommets // valeur – poids de e sortante
         // Exemple: (1,7) = 1 => b; e = (a,b) poids 7 
-        TreeMap<String, Edge> edges;
+        TreeSet<Edge> edges;
         // TreeMap aretesEntrantes; //optionel
-        public Vertex(String v, TreeMap<String, Edge> edges) {
+        public Vertex(String v, TreeSet<Edge> edges) {
             this.v = v;
             this.edges = edges;
         }
+
+        public void  addEdge(String name,Vertex start, Vertex end, Integer weight){
+            if(edges == null){
+                edges = new TreeSet<Edge>();
+            }
+            edges.add(new Edge(name,start,end,weight));
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if(this == o){
+                return true;
+            }
+            if(!(o instanceof Vertex)){
+                return false;
+            }
+
+            Vertex ver = (Vertex) o;
+
+            return this.v == ver.v;
+            
+        }
     };
 
-    private int curIndex = 0;
-    //clé – sommet, valeur – indice de sommet 
-    private TreeMap<String, Integer> indexes;
-    private ArrayList<Vertex> vertexs;
+   
     
-    public void addVertex(String s) {
+    // public void addVertex(String s) {
 
-        this.indexes.put(s, curIndex);
+    //     this.indexes.put(s, curIndex);
         
-        Vertex vertex = new Vertex(s, new TreeMap<>());
-        this.vertexs.add(vertex);
+    //     Vertex vertex = new Vertex(s, new TreeMap<>());
+    //     this.vertexs.add(vertex);
 
-        curIndex++;
+    //     curIndex++;
 
+    // }
+    public void addVertex(String v){
+        this.vertexTree.add(v);
+        Vertex vertex = new Vertex(v,new TreeSet<>());
+        this.vertexsEdges.add(vertex);
     }
 
-    public void addEdge(String name, String first, String second, int value ){
+
+    public void addEdge(String name, Vertex first, Vertex second, int value ){
         Edge edge = new Edge(name, first, second, value);
         // this.vertexs.g
     }
+
+   public ArrayList<Vertex>  getVertexsEdges(){
+        return this.vertexsEdges;
+   }
         
 }
